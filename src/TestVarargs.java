f1c1e0a9332c620493965487afbca9e3cefcfcf5
JakubLenart <70464589+JakubLenart@users.noreply.github.com>
2020-09-01 20:57:37 +0200

    added out/ and modified src/TestVarargs.java

--- src/TestVarargs.java
+++ src/TestVarargs.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class TestVarargs {
-}
+
+    public static void printAllArguments(String greetingSeparator, Object... arguments)
+    { System.out.println(greetingSeparator);
+            for(int i=0; i<arguments.length; i++) {
+                System.out.println(arguments[i]);
+            }
+        }
+        public static void main(String[] args) {
+            printAllArguments("----", 1);
+            printAllArguments( "--***--",1,2);
+            printAllArguments(  "*****", 1,2, "Mickey Mouse");
+            Object[] array1 = {1,2, "Mickey Mouse using array as argument"};
+            printAllArguments("@@@@@@@@@@@@@@@@", array1);
+        }
+    }
+
+
+
+
+
