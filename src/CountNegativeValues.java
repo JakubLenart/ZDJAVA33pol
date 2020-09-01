8f69a7b17ca518a4b9d1be4204fca6b61c6b0467
JakubLenart <70464589+JakubLenart@users.noreply.github.com>
2020-09-01 21:04:00 +0200

    added CountNegativeValues class

---
+++ src/CountNegativeValues.java
@@ -0,0 +1,19 @@
+public class CountNegativeValues {
+
+    public static void countNegativeValues(int[] allValues) {
+            int counter = 0;
+            for(int value: allValues){
+                if(value < 0){
+                    counter++;
+                }
+            }
+            System.out.println("Number of negative equals: " + counter);
+        }
+        public static void main(String[] args) {
+            int[] allValues = {1, 3,-10, 3, 4, -1, -2};
+            countNegativeValues(allValues);
+        }
+    }
+
+
+
